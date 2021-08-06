25cfd64ad6b200cdbd79c510a98b3d3a344e2112
erick_braga <@Erickmatheus07>
2021-08-06 10:22:14 -0300

    Curso finalizado

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,6 +7,21 @@ class FormularioCadastro extends Component {
     this.titulo ="";
     this.texto ="";
     this.categoria= "Sem Categoria";
+    this.state = {categorias: []};
+
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categoria.desinscrever(this._novasCategorias)
+  }
+
+  _novasCategorias(categorias){
+    this.setState({ ...this.state, categorias });
   }
 
   _handleMudancaCategoria(evento) {
@@ -40,8 +55,8 @@ class FormularioCadastro extends Component {
         className="form-cadastro-input"
         onChange={this._handleMudancaCategoria.bind(this)}
         >
-          <option value="">Sem Categoria</option>
-          {this.props.categorias.map((categoria, index) => {
+          <option>Sem Categoria</option>
+          {this.state.categorias.map((categoria, index) => {
             return <option key={index}>{categoria}</option>
           })}
         </select>
